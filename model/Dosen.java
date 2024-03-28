07cb09d740326aec6af619d7fdcc49273d3c910a
alwan <alwanfadhil39@gmail.com>
2024-03-28 11:33:05 +0800

    Memanggil method dari Class di package lain

---
+++ model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ publc static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Dosen ======================");
+  System.out.println("NIDN : 1103058602");
+  System.out.println("Nama : Mirza Yogy Kurniawan");
+  System.out.println("====================================");
+ }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Matakuliah{
+ publc static void tampilanInfo(){
+  System.out.println();
+  System.out.println("==Data Mata Kuliah ======================");
+  System.out.println("NIDN : TIF3402");
+  System.out.println("Nama : Mirza Pemrograman Berbasis Objek 1");
+  System.out.println("===================================");
+ }
+}
